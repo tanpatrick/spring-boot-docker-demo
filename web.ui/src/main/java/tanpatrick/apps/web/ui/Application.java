035016fe4445ebdad9aa0f633f86448a67dde41b
Patrick Tan <tanpatrickjoseph@gmail.com>
2017-05-01 21:58:52 +1200

    [web.ui] Add ui for displaying data retrieved from service.person

---
+++ web.ui/src/main/java/tanpatrick/apps/web/ui/Application.java
@@ -0,0 +1,16 @@
+package tanpatrick.apps.web.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Patrick Tan <tanpatrickjoseph@gmail.com>
+ */
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
